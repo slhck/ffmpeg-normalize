9728ae38de8b6374df2f261c8fa7ff6fd96e91df
Werner Robitza <werner.robitza@gmail.com>
2017-04-02 19:42:02 +0200

    use Markdown instead of RST for README/HISTORY

---
+++ HISTORY.md
@@ -0,0 +1,83 @@
+# History
+
+0.4.3 (2017-02-27)
+------------------
+
+-   Fix option `-np`, should be `-x` short
+-   Abort when input and output file are the same (ffmpeg can't
+    overwrite it)
+
+0.4.2 (2017-02-27)
+------------------
+
+-   Map metadata from input to output when merging
+-   Clarify use of merge option
+
+0.4.1 (2017-02-13)
+------------------
+
+-   Fix #13
+
+0.4 (2017-01-24)
+----------------
+
+-   Cleanup in code, make it class-based
+-   Drop avconv support, it was never good anyway
+-   Add support for specifying codec for non-merging operations
+-   Add support for specifying output format
+-   README improvements
+
+0.3 (2017-01-19)
+----------------
+
+-   Add option to remove prefix
+
+0.2.4 (2016-10-27)
+------------------
+
+-   Fixed issue where multiple spaces were collapsed into one
+
+0.2.3 (2016-02-12)
+------------------
+
+-   Fixed issue where ffmpeg could not be found if path included spaces
+
+0.2.2 (2016-02-09)
+------------------
+
+-   Change default level back to -26
+
+0.2.1 (2016-02-08)
+------------------
+
+-   Documentation fixes
+
+0.2.0 (2016-02-08)
+------------------
+
+-   Support multiple input files
+-   Allow merging with input file instead of creating separate WAV
+-   Write to directory instead of using prefix
+-   Set the audio codec when merging
+-   Set additional encoder or ffmpeg options
+
+Note: avconv support is very limited, use the real ffmpeg from
+<http://ffmpeg.org/> instead.
+
+0.1.3 (2015-12-15)
+------------------
+
+-   Bugfix for detecting ffmpeg or avconv on Windows (as .exe)
+-   Add version to Usage message
+-   Update year
+
+0.1.2 (2015-11-13)
+------------------
+
+-   Bugfix for missing ffmpeg or avconv
+
+0.1.0 (2015-08-01)
+------------------
+
+-   First release, changing name to ffmpeg-normalize
+
--- setup.py
+++ setup.py
@@ -1,52 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
-with open('README.rst', encoding='utf8') as readme_file:
-    readme = readme_file.read()
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-with open('HISTORY.rst', encoding='utf8') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+# Get the history from the HISTORY file
+with open(path.join(here, 'HISTORY.md'), encoding='utf-8') as f:
+    history = f.read()
 
-requirements = [
-    'docopt',
-]
-
-test_requirements = [
-    # 'pytest',
-]
+try:
+    import pypandoc
+    long_description = pypandoc.convert_text(long_description, 'rst', format='md')
+    history = pypandoc.convert_text(history, 'rst', format='md')
+except ImportError:
+    print("pypandoc module not found, could not convert Markdown to RST")
 
 import ffmpeg_normalize
 
 setup(
     name='ffmpeg-normalize',
     version=ffmpeg_normalize.__version__,
-    description="Normalize audio via ffmpeg / avconv",
-    long_description=readme + '\n\n' + history,
+    description="Normalize audio via ffmpeg",
+    long_description=long_description + '\n\n' + history,
     author="Werner Robitza",
     author_email='werner.robitza@gmail.com',
     url='https://github.com/slhck/ffmpeg-normalize',
-    packages=[
-        'ffmpeg_normalize',
-    ],
+    packages=['ffmpeg_normalize'],
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=["docopt"],
     license="MIT",
     zip_safe=False,
     keywords='avconv, ffmpeg, libav, normalize, audio',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Audio',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    # test_suite='tests',
-    # cmdclass={'test': PyTest},
-    # tests_require=test_requirements,
     entry_points={
         'console_scripts': [
             'ffmpeg-normalize = ffmpeg_normalize.__main__:main'
