slhck/ffmpeg-normalize

commit 304e8dfdebd32d49f036dfad9a8e9bdb7b5b852e

fix issue where output was wrong format

ffmpeg_normalize/__main__.py
@@ -201,7 +201,7 @@ class InputFile(object):
         """
 
         # by default, output filename is the input filename
-        self.output_filename = self.filename
+        self.output_filename = os.path.splitext(os.path.basename(self.filename))[0] + "." + self.format
 
         # prefix is disabled, so we need to make sure we're not writing ot a directory
         if self.no_prefix:

commit 6ab49ea7f686fd319cf5398fded28d13d14447e8

fix for problem introduced in 304e8df

ffmpeg_normalize/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.5"
+__version__ = "0.5.1"

ffmpeg_normalize/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-ffmpeg-normalize 0.5
+ffmpeg-normalize 0.5.1
 
 ffmpeg script for normalizing audio.
 
@@ -200,8 +200,12 @@ class InputFile(object):
         Set all the required output filenames and paths
         """
 
-        # by default, output filename is the input filename
-        self.output_filename = os.path.splitext(os.path.basename(self.filename))[0] + "." + self.format
+        if self.merge:
+            # when merging, output file is the same as input file
+            self.output_filename = self.filename
+        else:
+            # by default, output filename is the input filename, plus the format chosen (default: WAV)
+            self.output_filename = os.path.splitext(self.filename)[0] + "." + self.format
 
         # prefix is disabled, so we need to make sure we're not writing ot a directory
         if self.no_prefix: