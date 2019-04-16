7f9290434b3829874749828ea94d7c75e551e6b8
Werner Robitza <werner.robitza@gmail.com>
2019-04-16 10:27:40 +0200

    add release script

---
+++ release.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Bump the version, run auto-changelog, and push to Git
+#
+# Based on:
+# - https://gist.github.com/pete-otaqui/4188238
+# - https://gist.github.com/mareksuscak/1f206fbc3bb9d97dec9c
+
+command -v auto-changelog >/dev/null 2>&1 || { echo >&2 "auto-changelog is not installed. Install via npm!"; exit 1; }
+
+set -e
+
+NOW="$(date +'%B %d, %Y')"
+RED="\033[1;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
+LATEST_HASH=`git log --pretty=format:'%h' -n 1`
+
+QUESTION_FLAG="${GREEN}?"
+WARNING_FLAG="${YELLOW}!"
+NOTICE_FLAG="${CYAN}❯"
+
+PUSHING_MSG="${NOTICE_FLAG} Pushing new version to the ${WHITE}origin${CYAN}..."
+
+VERSION_FILE="ffmpeg_normalize/_version.py"
+
+BASE_STRING=$(grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' "$VERSION_FILE")
+BASE_LIST=(`echo $BASE_STRING | tr '.' ' '`)
+V_MAJOR=${BASE_LIST[0]}
+V_MINOR=${BASE_LIST[1]}
+V_PATCH=${BASE_LIST[2]}
+echo -e "${NOTICE_FLAG} Current version: ${WHITE}$BASE_STRING"
+echo -e "${NOTICE_FLAG} Latest commit hash: ${WHITE}$LATEST_HASH"
+V_MINOR=$((V_MINOR + 1))
+V_PATCH=0
+SUGGESTED_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+echo -ne "${QUESTION_FLAG} ${CYAN}Enter a version number [${WHITE}$SUGGESTED_VERSION${CYAN}]: "
+read INPUT_STRING
+if [ "$INPUT_STRING" = "" ]; then
+    INPUT_STRING=$SUGGESTED_VERSION
+fi
+echo -e "${NOTICE_FLAG} Will set new version to be ${WHITE}$INPUT_STRING"
+
+perl -pi -e "s/\Q$BASE_STRING\E/$INPUT_STRING/" "$VERSION_FILE"
+
+auto-changelog
+
+echo -e "$PUSHING_MSG"
+git add CHANGELOG.md "$VERSION_FILE"
+git commit -m "Bump version to ${INPUT_STRING}."
+git tag -a -m "Tag version ${INPUT_STRING}." "v$INPUT_STRING"
+
+git push && git push --tags
