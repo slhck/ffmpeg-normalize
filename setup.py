e79d59a343081269da084b716c68ce68c7f11052
Werner Robitza <werner.robitza@gmail.com>
2020-03-15 16:53:41 +0100

    python 3.8

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     entry_points={
         'console_scripts': [
