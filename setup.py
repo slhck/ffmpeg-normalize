49fe5e040b518f94c7a1707f9a73b3d28e4e34d8
Werner Robitza <werner.robitza@gmail.com>
2021-03-06 13:18:06 +0100

    remove support for older versions

--- setup.py
+++ setup.py
@@ -43,8 +43,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
