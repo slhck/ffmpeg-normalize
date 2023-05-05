35de9d13217c89fc324e046abe149da88a745818
Werner Robitza <werner.robitza@gmail.com>
2023-05-05 08:45:33 +0200

    fix test for progress
    seems flaky, worked locally?

--- test/test.py
+++ test/test.py
@@ -362,7 +362,8 @@ class TestFFmpegNormalize:
 
     def test_progress(self):
         _, stderr = ffmpeg_normalize_call(["test/test.mp4", "-pr"])
-        assert "100/100" in stderr
+        assert "0/100" in stderr
+        assert "100/100" in stderr or "100.0%" in stderr
         assert os.path.isfile("normalized/test.mkv")
 
     def test_duration(self):
