2a2639fa60f178cd76ec1a656e3e7c1913d16e3a
Werner Robitza <werner.robitza@gmail.com>
2024-11-18 11:31:19 +0100

    chore: add python 12

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires=">=3.8",
     entry_points={
